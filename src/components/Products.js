80c3b0e030f3bd04502eff4a12a068c208d89959
ShafeequeAli <shafeeeeque@gmail.com>
2020-10-07 14:59:15 +0530

    modification master

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,7 +5,7 @@ import Modal from "react-modal";
 
 export default class Products extends Component {
   constructor(props) {
-    super(props);
+    super();
     this.state = {
       product: null,
     };
@@ -18,6 +18,7 @@ export default class Products extends Component {
   };
   render() {
     const { product } = this.state;
+
     return (
       <div>
         <Fade bottom cascade>
